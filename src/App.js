0607d2b30ba651e2e11114cb49450ad1c07c2041
wangdengduo <286867796@qq.com>
2019-04-10 22:54:17 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React, { Component, createStore } from 'react';
-import { List, Typography,Skeleton, Tag } from 'antd';
-import { Modal, Button,Popconfirm ,message} from 'antd';
-import { Input,Form, Icon, Checkbox, } from 'antd';
+import React, { Component } from 'react';
+import { createStore } from 'redux'
+import { List, Typography, Skeleton, Tag } from 'antd';
+import { Modal, Button, Popconfirm, message } from 'antd';
+import { Input, Form, Icon, Checkbox, } from 'antd';
 import './App.css';
 import { Editor } from 'react-draft-wysiwyg';
 import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+function reducer(state = {
+  visible: false,
+}, action) {
+  if (typeof state === "undefined") {
+    return 0;
+  }
+  switch (action.type) {
+    case 'SHOW':
+      return {
+        ...state,
+        visible: true,
+      }
+    case 'HIDE':
+      return{
+        ...state,
+        visible: false,
+      }
+    default:
+      return console.log("default success");
+  }
+}
+
+var store = createStore(reducer);
+
+var modalActionCreator = function (isShow) {
+  return {
+    type: isShow?'SHOW':'HIDE',
+  }
+}
+
+
+
 
 const Search = Input.Search;
 
@@ -33,13 +67,13 @@ class NormalLoginForm extends React.Component {
         })
           .then(
             result => {
-            if (result.status === 0) {
-              message.success("登录成功");
-              this.props.hideLogin();
+              if (result.status === 0) {
+                message.success("登录成功");
+                this.props.hideLogin();
+              }
+              console.log("登录成功")
             }
-            console.log("登录成功")
-          }
- 
+
           );
       }
     });
@@ -91,43 +125,48 @@ class App extends Component {
       mydata: [
       ],
       inputValue: "",
-      inputIndex:undefined,
+      inputIndex: undefined,
       visible: false,
-      loginVisible:false,
-      modifyList:false,
-      addList:false,
+      loginVisible: false,
+      modifyList: false,
+      addList: false,
       editorState: EditorState.createEmpty(),
-      searchValue:"",
-      userName:"",
+      searchValue: "",
+      userName: "",
     };
+    store.subscribe(()=>{
+      this.setState({
+        visible:store.getState().visible,
+      });
+    });
     this.showAddModal = this.showAddModal.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
     this.catchData = this.catchData.bind(this);
     this.handleOk = this.handleOk.bind(this);
     this.cancel = this.cancel.bind(this);
-    this.onEditorStateChange=this.onEditorStateChange.bind(this);
-    this.userLogin=this.userLogin.bind(this);
-    this.loginHandleCancel=this.loginHandleCancel.bind(this);
-    this.loginHandleOk=this.loginHandleOk.bind(this);
-    this.hideLogin=this.hideLogin.bind(this);
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.userLogin = this.userLogin.bind(this);
+    this.loginHandleCancel = this.loginHandleCancel.bind(this);
+    this.loginHandleOk = this.loginHandleOk.bind(this);
+    this.hideLogin = this.hideLogin.bind(this);
   };
 
-  hideLogin(){
+  hideLogin() {
     this.setState({
-      loginVisible:false,
+      loginVisible: false,
     })
   }
 
-  userLogin(){
+  userLogin() {
     this.setState({
-      loginVisible:true,
+      loginVisible: true,
     })
   }
-  onSearch(value){
-    this.state.searchValue=value;
-    
+  onSearch(value) {
+    this.state.searchValue = value;
+
     this.setState({
-      searchValue:this.state.searchValue,
+      searchValue: this.state.searchValue,
     })
     this.getData();
   }
@@ -151,25 +190,26 @@ class App extends Component {
             mydata: this.state.mydata
           })
         }
-      });    
+      });
   }
-  editList(idx, e){
+  editList(idx, e) {
     console.log(htmlToDraft(this.state.mydata[idx].content));
-    const contentBlock=htmlToDraft(this.state.mydata[idx].content);
+    const contentBlock = htmlToDraft(this.state.mydata[idx].content);
     if (contentBlock) {
       const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
       const editorState = EditorState.createWithContent(contentState);
       this.setState({
-        visible: true,
+        // visible: true,
         modifyList: true,
-        inputValue:this.state.mydata[idx].title,
-        editorState:editorState,
-        inputIndex:idx,
-      }); 
+        inputValue: this.state.mydata[idx].title,
+        editorState: editorState,
+        inputIndex: idx,
+      });
+      store.dispatch(modalActionCreator(true))
     }
-             
+
   }
-  
+
   cancel(e) {
     message.error('Click on No');
   }
@@ -183,36 +223,38 @@ class App extends Component {
   showAddModal() {
 
     this.setState({
-       visible: true,
-       addList:true, 
-       inputValue:"",
-       editorState:EditorState.createEmpty(),
-      })
+      // visible: true,
+      addList: true,
+      inputValue: "",
+      editorState: EditorState.createEmpty(),
+    })
+    store.dispatch(modalActionCreator(true))
   }
-  loginHandleCancel(){
+  loginHandleCancel() {
     this.setState({
-      loginVisible:false,
+      loginVisible: false,
     })
   }
   handleCancel() {
-    this.setState({ visible: false })
+    // this.setState({ visible: false })
+    store.dispatch(modalActionCreator(false))
   }
   catchData(e) {
-    this.setState({ 
-      inputValue:e.target.value,
+    this.setState({
+      inputValue: e.target.value,
     });
   }
-  loginHandleOk(){
+  loginHandleOk() {
     this.setState({
       loginVisible: false,
     });
   }
   handleOk() {
-    console.log(this.state.addList,this.state.modifyList);
-    if (this.state.addList) {      
+    console.log(this.state.addList, this.state.modifyList);
+    if (this.state.addList) {
       let formData = new FormData();
       formData.append('title', this.state.inputValue);
-      formData.append('content',draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())));
+      formData.append('content', draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())));
 
       fetch(`https://www.yhddx.cn/fe_demo/add/`, {
         method: 'POST',
@@ -222,25 +264,26 @@ class App extends Component {
       }).then(function (response) {
         return response.json();
       })
-      .then(result=>{
-        if(result.status===0){
-          message.success("提交成功");
-          this.state.mydata.push({ id:result.id, title: this.state.inputValue,content:draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())) });
+        .then(result => {
+          if (result.status === 0) {
+            message.success("提交成功");
+            this.state.mydata.push({ id: result.id, title: this.state.inputValue, content: draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())) });
 
-          this.setState({
-            mydata: this.state.mydata,
+            this.setState({
+              mydata: this.state.mydata,
 
-            visible: false,
-            addList: false,
-          });
-        }
-      });    
+              // visible: false,
+              addList: false,
+            });
+            store.dispatch(modalActionCreator(false));
+          }
+        });
     }
     if (this.state.modifyList) {
       let formData = new FormData();
       formData.append('title', this.state.inputValue);
-      formData.append('content',draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())));
-      formData.append('id',this.state.mydata[this.state.inputIndex].id);
+      formData.append('content', draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())));
+      formData.append('id', this.state.mydata[this.state.inputIndex].id);
 
       fetch(`https://www.yhddx.cn/fe_demo/edit/`, {
         method: 'POST',
@@ -257,28 +300,29 @@ class App extends Component {
             this.state.mydata[this.state.inputIndex].content = draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()));
 
             this.setState({
-              visible: false,
+              // visible: false,
               modifyList: false,
               mydata: this.state.mydata,
             });
+            store.dispatch(modalActionCreator(false));
           }
         })
         .catch(function (error) {
           console.log(error);
-        });         
+        });
 
+    }
   }
-}
   getData() {
-    console.log(`https://www.yhddx.cn/fe_demo/?q=`+this.state.searchValue);
-    fetch(`https://www.yhddx.cn/fe_demo/?q=`+this.state.searchValue, {credentials: 'include',})
+    console.log(`https://www.yhddx.cn/fe_demo/?q=` + this.state.searchValue);
+    fetch(`https://www.yhddx.cn/fe_demo/?q=` + this.state.searchValue, { credentials: 'include', })
       .then(function (response) {
         return response.json();
       })
       .then(function (myJson) {
         this.setState({
           mydata: myJson.list,
-          userName:myJson.username,
+          userName: myJson.username,
         });
       }.bind(this)
       )
@@ -295,11 +339,11 @@ class App extends Component {
 
   render() {
 
-    const data = this.state.mydata.map(function(ele, index){
+    const data = this.state.mydata.map(function (ele, index) {
       return ele.title;
     });
-    const { editorState } = this.state; 
-   
+    const { editorState } = this.state;
+
     return (
 
       <div>
@@ -312,15 +356,15 @@ class App extends Component {
         <Tag>{this.state.userName}</Tag>
         <Button className="userLogin" type="primary" onClick={this.userLogin} >点击登录</Button>
         <Modal
-            title="Log in"
-            visible={this.state.loginVisible}
-            footer={null}
-            onCancel={this.loginHandleCancel}
-            width={350}
-          >
-          <WrappedNormalLoginForm hideLogin={this.hideLogin}/>
-          
-          </Modal>
+          title="Log in"
+          visible={this.state.loginVisible}
+          footer={null}
+          onCancel={this.loginHandleCancel}
+          width={350}
+        >
+          <WrappedNormalLoginForm hideLogin={this.hideLogin} />
+
+        </Modal>
         <br /><br />
         <h3 style={{ margin: '16px 0' }}>Large Size</h3>
         <List
@@ -332,20 +376,21 @@ class App extends Component {
           renderItem={(item, idx) => {
             return (
               <List.Item actions={[
-              <a onClick={this.editList.bind(this,idx)}>edit</a>, 
-              <Popconfirm title="Are you sure delete this task?" onConfirm={this.confirm.bind(this,idx)} onCancel={this.cancel} okText="Yes" cancelText="No">
-                <a href="#">Delete</a>
-              </Popconfirm>
+                <a onClick={this.editList.bind(this, idx)}>edit</a>,
+                <Popconfirm title="Are you sure delete this task?" onConfirm={this.confirm.bind(this, idx)} onCancel={this.cancel} okText="Yes" cancelText="No">
+                  <a href="#">Delete</a>
+                </Popconfirm>
               ]}>
-              <Skeleton title={false} active loading={false}>
-                <List.Item.Meta
-                  title={<a href="https://ant.design">{item}</a>}
-                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                />
-                <div>content</div>
-              </Skeleton>
-            </List.Item>)}
-            }
+                <Skeleton title={false} active loading={false}>
+                  <List.Item.Meta
+                    title={<a href="https://ant.design">{item}</a>}
+                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  />
+                  <div>content</div>
+                </Skeleton>
+              </List.Item>)
+          }
+          }
         />
         <div>
           <Button type="primary" onClick={this.showAddModal}>Add</Button>
@@ -356,11 +401,11 @@ class App extends Component {
             onCancel={this.handleCancel}
           >
             <Input placeholder="Basic usage" onChange={this.catchData} value={this.state.inputValue} />
-            <Editor 
-            editorState={editorState}
-            wrapperClassName="demo-wrapper"
-            editorClassName="demo-editor"
-            onEditorStateChange={this.onEditorStateChange}
+            <Editor
+              editorState={editorState}
+              wrapperClassName="demo-wrapper"
+              editorClassName="demo-editor"
+              onEditorStateChange={this.onEditorStateChange}
             />
           </Modal>
         </div>
